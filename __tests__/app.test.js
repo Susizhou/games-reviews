667ad1b02df501388967ac686650e0e88562b3f0
Susizhou <susanachen2000@gmail.com>
2022-11-14 12:05:43 +0000

    test husky again

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,7 +14,6 @@ afterAll(() => {
 
 describe("/ap/categories", () => {
   describe("get request", () => {
-    
     test("shoul return all categories with the with the following column properties: slug and description", () => {
       return request(app)
         .get("/api/categories")
@@ -42,6 +41,5 @@ describe("/ap/categories", () => {
             expect(body.msg).toBe('Route not found')
         })
     });
-
   });
 });
